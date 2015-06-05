115a40f343c45a677f576486a881c40e636a4e40
Bokai YU <yubokai8@gmail.com>
2015-06-05 12:33:27 +0200

    voila

--- index.php
+++ index.php
@@ -1,37 +1,14 @@
 <?php
 session_start();//
 
-//connect to data base
-$host="localhost";
-$port="5432";
-$dbName="postgres";
-$user="postgres";
-$password="asd123zxc";
-
-//BDD du UTC
-// $host="tuxa.sme.utc";
-// $port="5432";
-// $dbName="dbnf17p051";
-// $user="nf17p051";
-// $password="kjsCCyJ1";
-
-$dsn = "pgsql:host=$host;port=$port;dbname=$dbName;user=$user;password=$password";
-
-try{
-  // create a PostgreSQL database connection
-  $conn = new PDO($dsn);
-}catch (PDOException $e){
-  // report error message
-  echo $e->getMessage();
-}
-
 include 'Vue/header_base.php';
+// echo (isset($_POST['action']));
 
 include 'Control/connection.php';
 
-if(isset($_GET['modele']))
+if(isset($_REQUEST['modele']))
 {
-	include 'Control/'.$_GET['modele'].'.php';
+	include 'Control/'.$_REQUEST['modele'].'.php';
 }
 else 
 {
